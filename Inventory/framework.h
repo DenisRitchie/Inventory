3918faafce8dc7d54178967f3df5d91c2ccb5263
Denis West <DenisWest@outlook.com>
2026-05-07 00:15:59 +0200

    feat: Initial Project

---
+++ Inventory/framework.h
@@ -0,0 +1,25 @@
+// header.h: archivo de inclusión para archivos de inclusión estándar del sistema,
+// o archivos de inclusión específicos de un proyecto
+//
+
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN   // Excluir material rara vez utilizado de encabezados de Windows
+
+// Archivos de encabezado de Windows
+#include <Windows.h>
+
+// Archivos de encabezado en tiempo de ejecución de C
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+// Archivos de encabezado en tiempo de ejecución de C++
+#include <format>
+#include <string>
+#include <string_view>
+#include <algorithm>
+
+// Archivos de encabezado en tiempo de ejecución externos
